4aac8023b52a46e5c0a91918d5197f4644df4193
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-31 11:44:42 +0000

    created user class that gets initialized with a name

---
+++ tests/userModel.test.js
@@ -0,0 +1,11 @@
+const User = require('../lib/userModel.js')
+
+describe('userModel', () => {
+
+  test('model stores name of User', () => {
+    let user = new User({name: 'Test'});
+    expect(user.getName()).toBe('Test')
+  })
+
+
+});
