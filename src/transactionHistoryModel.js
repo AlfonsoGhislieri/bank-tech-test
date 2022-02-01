e4332ab908c1783093dfa039344ba6602fddb8d0
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-01 12:24:25 +0000

    refactoring viewStatements using map instead of for in loop

--- src/transactionHistoryModel.js
+++ src/transactionHistoryModel.js
@@ -6,21 +6,21 @@ class TransactionHistory {
   addTransaction = (transaction) => this._transactions.push(transaction);
 
   viewStatements = () => {
-    let result = 'date || credit || debit || balance\n'
-    for (const transaction in this._transactions.reverse()) {
-      const statement = this.#createStatement(this._transactions[transaction]);
-      result += statement 
-    }
-    return result
-  }
+    let result = 'date || credit || debit || balance\n';
+
+    result += this._transactions.reverse()
+        .map( (elem) => this.#createStatement(elem)).join('');
+
+    return result;
+  };
 
   #createStatement = (transaction) => {
-    const date = transaction['date']
-    const credit = transaction['credit']
-    const debit = transaction['debit']
-    const balance = transaction['balance']
-    return (`${date} || ${credit} || ${debit} || ${balance}\n`)
-  }
+    const date = transaction['date'];
+    const credit = transaction['credit'];
+    const debit = transaction['debit'];
+    const balance = transaction['balance'];
+    return (`${date} || ${credit} || ${debit} || ${balance}\n`);
+  };
 }
 
 module.exports = TransactionHistory;
