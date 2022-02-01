5e503c13b8a7529bceaef66b395c923b5f9245da
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-01 09:30:02 +0000

    transaction model stores balance and date, froze time for tests

--- src/transactionModel.js
+++ src/transactionModel.js
@@ -1,7 +1,14 @@
 class Transaction {
-  constructor({amount: amount, transactionType: type}) {
+  constructor({
+    amount: amount, 
+    transactionType: type, 
+    balance: balance,
+    date: date}) {
+    
     this._amount = amount;
     this._type = type;
+    this._balance = balance;
+    this._date = date;
   }
 }
 
--- tests/transactionModel.test.js
+++ tests/transactionModel.test.js
@@ -1,19 +1,35 @@
 const Transaction = require('../src/transactionModel.js');
+var timekeeper = require('timekeeper');
+var time = new Date(1643707675097);
+timekeeper.freeze(time);
 
 describe('Transaction', () => {
+  const mockDate = new Date().toLocaleDateString()
   
   describe('initialization', () => {
     test('an instance of a transaction stores a deposit', () => {
-      const transaction = new Transaction({amount: 500, transactionType: "deposit"});
+      const transaction = new Transaction({
+        amount: 500, 
+        transactionType: "deposit",
+        balance: 500,
+        date: mockDate });
 
       expect(transaction._amount).toEqual(500);
       expect(transaction._type).toEqual("deposit");
+      expect(transaction._balance).toEqual(500)
+      expect(transaction._date).toEqual("01/02/2022")
     });
     test('an instance of a transaction stores a withdrawal', () => {
-      const transaction = new Transaction({amount: 500, transactionType: "withdraw"});
+      const transaction = new Transaction({
+        amount: 500, 
+        transactionType: "withdraw",
+        balance: 0,
+        date: mockDate });
 
       expect(transaction._amount).toEqual(500);
       expect(transaction._type).toEqual("withdraw");
+      expect(transaction._balance).toEqual(0)
+      expect(transaction._date).toEqual("01/02/2022")
     });
   });
 });
